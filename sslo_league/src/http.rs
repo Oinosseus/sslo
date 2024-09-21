113b7f717bd0d174f5e67ac8da0ee9950984a186
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2024-09-21 18:23:14 +0200

    begin registration form

--- sslo_league/src/http.rs
+++ sslo_league/src/http.rs
@@ -12,6 +12,7 @@ pub mod route_html_login;
 struct HtmlTemplate {
     html_body: String,
     css_files: Vec<& 'static str>,
+    js_files: Vec<& 'static str>,
 }
 
 impl HtmlTemplate {
@@ -20,6 +21,7 @@ impl HtmlTemplate {
         HtmlTemplate {
             html_body: "".to_string(),
             css_files: Vec::new(),
+            js_files: Vec::neww(),
         }
     }
 
@@ -29,10 +31,14 @@ impl HtmlTemplate {
         self.html_body += body;
     }
 
-
     /// request a CSS file to be additionally loaded
     pub fn include_css(&mut self, file_path: & 'static str) {
         self.css_files.push(file_path)
+    }
+
+    /// request a javascript file to be additionally loaded
+    pub fn include_js(&mut self, file_path: & 'static str) {
+        self.js_files.push(file_path)
     }
 }
 
@@ -55,11 +61,11 @@ impl IntoResponse for HtmlTemplate {
             html += css_file;
             html += "\">\n";
         }
-        // for js_file in &self.js_files {
-        //     html += "    <script type=\"module\" src=\"";
-        //     html += js_file;
-        //     html += "\" defer></script>\n";
-        // }
+        for js_file in &self.js_files {
+            html += "    <script type=\"module\" src=\"";
+            html += js_file;
+            html += "\" defer></script>\n";
+        }
         // for js_file in active_page.javascript_files() {
         //     html += "    <script type=\"module\" src=\"";
         //     html += js_file;
--- sslo_league/src/http/route_html_login.rs
+++ sslo_league/src/http/route_html_login.rs
@@ -5,13 +5,36 @@ use crate::http::HtmlTemplate;
 pub async fn handler() -> Result<impl IntoResponse, StatusCode> {
     let mut html = HtmlTemplate::new();
     html.include_css("/rsc/css/login.css");
+    html.include_js("/rsc/js/login.js");
 
-    html.push_body("Local login");
+    // Tab Selection
+    html.push_body("<div id=\"TabSelection\">");
+    html.push_body("<button id=\"LoginSsloButton\">SSLO Login</button>");
+    html.push_body("<button id=\"RegisterSsloButton\">Register</button>");
+    html.push_body("</div>");
+
+
+    // SSLO Local Login
+    html.push_body("<div id=\"LoginSsloForm\">");
+    html.push_body("<label>Local SSLO login</label>");
     html.push_body("<form method=\"post\">");
     html.push_body("<input required autofocus placeholder=\"username\" type=\"text\" name=\"LoginUsername\">");
-    html.push_body("<input required placeholder=\"password\" type=\"password\" name=\"LoginUsername\">");
+    html.push_body("<input required placeholder=\"password\" type=\"password\" name=\"LoginPassword\">");
     html.push_body("<button type=\"submit\">Login</button>");
     html.push_body("</form>");
+    html.push_body("</div>");
+
+    // SSLO Local Registration
+    html.push_body("<div id=\"RegisterSsloForm\"");
+    html.push_body("<label>Local SSLO Registration</label>");
+    html.push_body("<form method=\"post\">");
+    html.push_body("<input required autofocus placeholder=\"username\" type=\"text\" name=\"LoginUsername\">");
+    html.push_body("<input required placeholder=\"password\" type=\"password\" name=\"LoginPassword\">");
+    html.push_body("<input required placeholder=\"verify password\" type=\"password\" name=\"LoginPassword2\">");
+    html.push_body("<button type=\"submit\">Register</button>");
+    html.push_body("</form>");
+    html.push_body("</div>");
+
 
     return Ok(html);
 }
