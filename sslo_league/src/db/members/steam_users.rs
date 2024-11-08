0ea1c5aaf830db4612f3536cd3e90d6dd0372127
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2024-11-08 20:19:15 +0100

    insert into steam_users

--- sslo_league/src/db/members/steam_users.rs
+++ sslo_league/src/db/members/steam_users.rs
@@ -41,4 +41,43 @@ impl SteamUser {
         if let Some(row) = rows.pop() { Some(Self {row, pool}) }
         else { None }
     }
+
+
+    pub async fn new_or_from_steam_id(pool: SqlitePool, steam_id: &str) -> Option<Self> {
+
+        // find steam id
+        let mut rows: Vec<DbRow> = match sqlx::query_as("SELECT rowid,* FROM steam_users WHERE steam_id = $1 LIMIT 2;")
+            .bind(steam_id)
+            .fetch_all(&pool)
+            .await {
+            Ok(r) => r,
+            Err(e) => {
+                log::error!("Failed to query database: {}", e);
+                return None;
+            }
+        };
+
+        // return when unique ID is found
+        if rows.len() > 1 {
+            log::error!("Ambiguous steam_id for db.members.steam_users.steam_id={}", steam_id);
+            return None;
+        } else if let Some(row) = rows.pop() {
+            return Some(Self{ row, pool});
+        }
+
+        // insert new
+        let row: DbRow = match sqlx::query_as("INSERT INTO steam_users (steam_id) VALUES ($1) RETURNING rowid,*;")
+            .bind(steam_id)
+            .fetch_one(&pool)
+            .await {
+            Ok(x) => x,
+            Err(e) => {
+                log::error!("Failed to insert into db.members.steam_users: {}", e);
+                return None;
+            }
+        };
+
+        // return
+        Some(Self{ row, pool})
+    }
 }
